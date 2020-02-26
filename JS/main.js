ad4976aa301d3b59e8984816dcfdb0a323ad2d9a
yang052413 <liyang0525.ly@gmail.com>
2020-02-26 12:22:54 -0800

    profile image can change realtime

--- JS/main.js
+++ JS/main.js
@@ -36,7 +36,6 @@ $(document).ready(function () {
     var dateTime = date + ' ' + time;
     console.log("现在的时间" + dateTime);
 
-
     //用户发送信息 User send message
     firebase.auth().onAuthStateChanged(function (user) {
         db.collection("user")
@@ -48,13 +47,14 @@ $(document).ready(function () {
             });
 
         $(".current-user-name").html(user.displayName);
+        $("#user-btn").attr("src", "Img/user/" + user.photoURL);
 
+        //用户按回车后write message to firebase
         $("#user-input").keyup(function (event) {
             var userMsg = $("#user-input").val();
             var userName = user.displayName;
+            var userPhoto = user.photoURL;
 
-            var x = db.collection("user").doc(user.uid);
-         
             if (event.keyCode == 13) {
                 if (userMsg == '') {
                     alert("内容不能为空");
@@ -65,23 +65,24 @@ $(document).ready(function () {
                         User: userName,
                         Date: dateTime,
                         Chat: userMsg,
-                        // Profile: x,
+                        Profile: userPhoto,
                     };
                     db.collection("chatRoom").add(docMsg);
-                    console.log(x);
+                    console.log(user.email);
                 }
             }
         });
-
     });
 
-
+    //当前在线人员的数量 之后改成onSnapshot
     let online_member_count = 0;
     db.collection("user").get().then(function (snap) {
         snap.forEach(function (doc) {
             var other_user_name = doc.data().name;
+            var other_user_profile = doc.data().profile;
+
             var user_wrap = $("<div class=user-wrap>" +
-                "<img class=user-profile-img id=user-btn src=Img/user/default-1.jpg>" +
+                "<img class=user-profile-img id=user-btn src=Img/user/" + other_user_profile + ">" +
                 "<div class=current-user-info>" +
                 "<img class=user-online-status src=Img/online-status.png width=12px height=12px>" +
                 "<p class=other-user-name>" + other_user_name + "</p>" +
@@ -102,10 +103,11 @@ $(document).ready(function () {
                 var userNameCloud = change.doc.data().User;
                 var dateCloud = change.doc.data().Date;
                 var chatCloud = change.doc.data().Chat;
+                var profileCloud = change.doc.data().Profile;
 
                 //DOM HTML element 
                 let user_chat = $("<div class=user-chat>" +
-                    "<img id=current-user-profile class=user-chat-profile src=Img/user/default-1.jpg>" +
+                    "<img class=user-chat-profile src=Img/user/" + profileCloud + ">" +
                     "<div class=user-chat-content-wrap>" +
                     "<p class=user-chat-user>" +
                     "<span class=user-name>" + userNameCloud + "</span>" +
@@ -114,31 +116,31 @@ $(document).ready(function () {
                     "<p class=user-chat-content>" + chatCloud + "</p>" +
                     "</div>" +
                     "</div>");
-
+   
                 //if the new object being added, append to chat room
                 if (change.type == "added") {
                     $(".main-room-chat").append(user_chat);
                     console.log("new chat");
+                    console.log(userNameCloud);
                 }
 
                 //回滚到最新的信息
                 setTimeout(function () {
                     ($('.main-room-chat').children(".user-chat:last-child")[0]).scrollIntoView();
                 }, 100);
-       
+
             });
         });
     });
 
-
     // 创建用户缩略图DOM并生成
-    for(var i = 1; i <= 35; i++) {
+    for (var i = 1; i <= 35; i++) {
         var profile_pic = $("<img id=profile-" + i + " class=profile-pic src=Img/user/profile-" + i + ".jpg></img>");
         $(".profile-pic-wrap").append(profile_pic);
     }
 
     //显示用户头像更改界面
-    $("#profile-btn").click(function() {
+    $("#profile-btn").click(function () {
         $("#profile-pic-modal").fadeIn();
     });
 
@@ -146,17 +148,29 @@ $(document).ready(function () {
     firebase.auth().onAuthStateChanged(function (user) {
         var profile_pic_path;
 
-        $(".profile-pic").click(function() {
+        $(".profile-pic").click(function () {
             profile_pic_path = $(this).attr("id") + ".jpg";
             $("#user-btn").attr("src", "Img/user/" + profile_pic_path);
 
+            var current_user = firebase.auth().currentUser;
+
+            current_user.updateProfile({
+                photoURL: profile_pic_path
+              }).then(function() {
+                console.log("user profile change successfully");
+              }).catch(function(error) {
+                console.log("cannot update user profile picture");
+              });
+
             db.collection("user").doc(user.uid).set({
-                "profile": profile_pic_path,
+                profile: profile_pic_path,
             }, {
                 merge: true
             });
             $("#profile-pic-modal").fadeOut();
         });
-       
+
     });
+
+
 });
